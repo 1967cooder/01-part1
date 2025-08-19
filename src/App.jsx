c1f9731c5c27ad93137f8040248864760bdf8da1
1967cooder <silvanalindholm@hotmail.com>
2025-08-19 09:34:53 +0300

    add 01-Part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-const App = () => {
-  return (
-  <>
-    <p>Hello world</p>
-  </>
-  );
-};
+import React from "react"
 
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+}
 export default App
